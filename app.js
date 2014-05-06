e72afa2478fef273352097c23c575c9045549f8d
Matthew Falbo <matthew.falbo@gmail.com>
2014-05-06 15:10:26 -0600

    Completed Exercise.  Create a server that will serve up an html page, take in a form submit, and redirect to a success page.

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var app = express();
 
+var fs = require('fs');
+
 app.get('/', function(req, res) {
 	res.send('<h1>Hello Boulder</h1>');
 });
+
+app.get('/hi', function(req, res){
+	res.send('You\'re now on the welcome page');
+});
+
+app.get('/bye', function(req,res){
+	res.send('You\'re now on the good-bye page');
+});
+
+app.get('/stay', function(req,res){
+	res.send('This is where you just hangout for a while');
+});
+
+app.get('/success', function(req, res){
+	res.send('You have sucessfully submitted your form!');
+});
+
+app.get('/form', function(req, res){
+	res.header('Content-Type','text/html');
+	fs.readFile('index.html', function(err, data){
+		res.send(data);
+	});
+});
+
+app.post('/formsubmit', function(req, res){
+	res.redirect('/success');
+});
+
+
 
 var server = app.listen(8959, function() {
 	console.log('Express server listening on port ' + server.address().port);
